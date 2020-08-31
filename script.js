609597e783620f82b203cf1f7b86e1df169f5486
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-08-31 17:50:47 +0100

    tv-project is revised with 200

--- script.js
+++ script.js
@@ -8,19 +8,22 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+  const bodyElement = document.querySelector("body");
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
+  const containerName = document.createElement("div");
+  containerName.className = "container";
+
+  const divRow = document.createElement("div");
+  divRow.className = "row";
+
+  bodyElement.appendChild(containerName);
+  containerName.appendChild(divRow);
 
   episodeList.forEach((episode) => {
 
-    const containerName = document.createElement("div");
-    containerName.className = "container";
-
-    const divRow = document.createElement("div");
-    divRow.className = "row";
-
     const divCard = document.createElement("div");
-    divCard.className = "card col-4";
+    divCard.className = "card col-3";
 
     const divCardHeader = document.createElement("div");
     divCardHeader.className = "card-header";
@@ -46,8 +49,7 @@ function makePageForEpisodes(episodeList) {
     cardFooterLink.href = "https://www.tvmaze.com/";
 
     // Appending the created elements
-    rootElem.appendChild(containerName);
-    containerName.appendChild(divRow);
+
     divRow.appendChild(divCard);
     divCard.appendChild(divCardHeader);
     divCardHeader.appendChild(episodeName);
@@ -63,19 +65,64 @@ window.onload = setup;
 
 // Search Bar creation and search function
 function searchTheEpisodes() {
+  const rootElem = document.getElementById("root");
+  const allEpisodes = getAllEpisodes();
+
   const searchNavBar = document.createElement("nav");
-  searchNavBar.className = "navbar navbar - light bg - light";
+  searchNavBar.className = "navbar navbar-light bg-light";
 
   const searchFormElement = document.createElement("form");
   searchFormElement.className = "form-inline";
 
   const inputElement = document.createElement("input");
   inputElement.className = "form-control mr-sm-2";
+  inputElement.setAttribute("type", "search");
+  inputElement.setAttribute("placeholder", "Search");
+  inputElement.setAttribute("aria-label", "Search");
+
+  console.log(inputElement);
   const inputSpanElement = document.createElement("span");
-  inputSpanElement.innerText = `Displaying ${episode.length} episodes match the current search`;
+  inputSpanElement.textContent = "Display how many episodes match the current search"
+  console.log(inputSpanElement);
 
 
+  rootElem.appendChild(searchNavBar);
+  searchNavBar.appendChild(searchFormElement);
+  searchFormElement.appendChild(inputElement)
+  searchFormElement.appendChild(inputSpanElement);
 
+  console.log(searchNavBar);
+
+  const episodeNames = [];
+  const episodeSummary = [];
+
+  allEpisodes.forEach((episode) => {
+    episodeNames.push(episode.name);
+    episodeSummary.push(episode.summary);
+  });
+
+  console.log(episodeNames);
+
+  function inputSelect(e) {
+    const addedInput = e.target.value.toLowerCase();
+    console.log(addedInput);
+    for (i = 0; i < episodeNames.length; i++) {
+      if (episodeNames[i].toLowerCase().indexOf(addedInput) !== -1 || episodeSummary[i].toLowerCase().indexOf(addedInput) !== -1) {
+
+        episodeNames[i].style.display = "block";
+        episodeSummary[i].style.display = "block";
+        inputSpanElement.textContent = `Displaying ${episodeNames[i].length} episodes match the current search`;
+
+      } else {
+        episodeNames[i].style.display = "none";
+        episodeSummary[i].style.display = "none";
+      }
+    }
+    console.log(addedInput);
+  }
+
+  inputElement.addEventListener("change", inputSelect);
+  console.log(inputElement);
 }
 
 
