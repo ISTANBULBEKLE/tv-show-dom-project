f2fbbaed848ec0cea8390e8ecb41e531296075d2
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-09-12 17:02:03 +0100

    revised

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 // This function is called with window onload calling other functions.
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  createInitialDivs();
-  // makePageForEpisodes(allEpisodes);
+  // getAllShows();
+  // console.log(getAllShows());
+  createAndSelectMenuForShows();
+  // fetchDataFromAPI();
   searchTheEpisodes();
   selectTheEpisodeFromList();
-  fetchDataFromAPI();
-
 }
 
-function createSecondSelectForShows() {
+function createAndSelectMenuForShows() {
+  const allShows = getAllShows();
+  console.log(getAllShows());
+  const showDiv = document.querySelector("#firstSelectDiv");
+  const selectShow = document.querySelector("#selectShows");
+  allShows.forEach((show) => {
+    const showOption = document.createElement("option");
+    showOption.setAttribute("value", "");
+    showOption.value = show.id;
+    showOption.innerText = show.name;
+    selectShow.appendChild(showOption);
+  });
 
+  /* const fetchTheShowFromAPI = (id) => {
+    fetch('https://api.tvmaze.com/shows/${id}/episodes');
+    .then(response => response.json());
+    .then(data => fetchDataFromAPI(data));
+  }
+
+  selectShow.addEventListener("change", selectFromShowMenu);
+
+  function selectFromShowMenu(event) {
+    let selectedShow = '';
+    if (event.target.value === "none") {
+      selectShow.innerHTML = "";
+      makePageForEpisodes(event);
+    } else {
+      selectedShow = allShows.filter((show) => {
+        return (`${show.name}` === selectedShow.value);
+      });
+      selectShow.innerHTML = "";
+      makePageForEpisodes(selectedShow);
+    };
+    selectedShow.value = "";
+  } */
 }
 
-
+/* const showList = document.querySelector('#selectShows').childNodes;
+console.log(showList); */
 
 ///////////// * This function is to fetch the data from the API and for calling the initial makePageForEpisodes ();
-const fetchDataFromAPI = () => {
-  fetch(' https://api.tvmaze.com/shows/82/episodes')
-    .then(response => response.json())
-    .then(data => onCallingTheData(data));
+
+const fetchDataFromAPI = (id) => {
+  fetch(" https://api.tvmaze.com/shows/82/episodes")
+    .then((response) => response.json())
+    .then((data) => onCallingTheData(data));
   // .catch(err => console.log(err));
-}
+};
 
 function onCallingTheData(data) {
   makePageForEpisodes(data);
@@ -29,16 +63,6 @@ function onCallingTheData(data) {
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-// This function creates initial 'container' (div), 'row' (div) and append them to 'rootElem'.
-function createInitialDivs() {
-  const rootElem = document.getElementById("root");
-  const containerName = document.createElement("div");
-  containerName.className = "container";
-  const divRow = document.createElement("div");
-  divRow.className = "row";
-  rootElem.appendChild(containerName);
-  containerName.appendChild(divRow);
-}
 // This function creates a card for each episode and grab them as a whole on screen with window.onload.
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
@@ -52,7 +76,11 @@ function makePageForEpisodes(episodeList) {
     divCardHeader.className = "card-header";
     const episodeName = document.createElement("h5");
     episodeName.className = "card-title";
-    episodeName.innerText = `${episode.name} - S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2,"0")}`;
+    episodeName.innerText = `${
+      episode.name
+    } - S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
     const episodeImage = document.createElement("img");
     episodeImage.className = "card-img-top";
     episodeImage.src = episode.image.medium;
@@ -77,10 +105,10 @@ function makePageForEpisodes(episodeList) {
 
 // This function search and list for input value that matches with the values in the 'card title' or 'card summary'.
 function searchTheEpisodes() {
-  const rootElem = document.getElementById("root");
-  const allEpisodes = getAllEpisodes();
-  const inputSearchElement = document.querySelector('#inputSearch');
-  const inputSpanElement = document.querySelector('.searchHolder');
+  const rootElem = document.querySelector("#root");
+  const allEpisodes = onCallingTheData();
+  const inputSearchElement = document.querySelector("#inputSearch");
+  const inputSpanElement = document.querySelector(".searchHolder");
 
   function inputSelect(e) {
     const addedInput = e.target.value.toLowerCase();
@@ -101,14 +129,18 @@ function searchTheEpisodes() {
 }
 // This function  selects episode and shows that episode only in the window.
 function selectTheEpisodeFromList() {
-  const allEpisodes = getAllEpisodes();
-  const secondSelectDiv = document.querySelector('#secondSelectDiv');
-  const selectTheEpisodes = document.querySelector('#selectEpisodes');
-  const cardContainer = document.querySelector('#cardContainer');
+  const allEpisodes = onCallingTheData();
+  const secondSelectDiv = document.querySelector("#secondSelectDiv");
+  const selectTheEpisodes = document.querySelector("#selectEpisodes");
+  const cardContainer = document.querySelector("#cardContainer");
 
   allEpisodes.forEach((episode) => {
     const optionElement = document.createElement("option");
-    optionElement.innerText = `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2,"0")} - ${episode.name}`;
+    optionElement.innerText = `S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
+      episode.name
+    }`;
     selectTheEpisodes.appendChild(optionElement);
   });
 
@@ -120,7 +152,13 @@ function selectTheEpisodeFromList() {
       makePageForEpisodes(allEpisodes);
     } else {
       const selectedEpisode = allEpisodes.filter((episode) => {
-        return (`S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${episode.name}` === selectTheEpisodes.value);
+        return (
+          `S${episode.season
+            .toString()
+            .padStart(2, "0")}E${episode.number
+            .toString()
+            .padStart(2, "0")} - ${episode.name}` === selectTheEpisodes.value
+        );
       });
       cardContainer.innerHTML = "";
       makePageForEpisodes(selectedEpisode);
