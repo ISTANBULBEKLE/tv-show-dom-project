be506d59aaf2eb6bca95e8203b0d4e6237b72577
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-09-13 23:54:45 +0100

    level 500 is revised

--- script.js
+++ script.js
@@ -1,13 +1,89 @@
 // This function is called with window onload calling other functions.
 
 function setup() {
-  getAllShows();
+  // getAllShows();
   createAndSelectMenuForShows();
+  createSummaryCardForShows();
   fetchEpisodesFromAPI();
   searchTheEpisodes();
   selectTheEpisodeFromList();
 }
 const selectShow = document.querySelector("#selectShows");
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// * This function will create a card for each show with mentioned information.
+
+function createSummaryCardForShows() {
+  const allShows = getAllShows();
+  const coverShowContainer = document.querySelector("#coverShowContainer");
+  const showContainer = document.querySelector("#showContainer");
+
+  allShows.forEach((show) => {
+    const showCard = document.createElement("div");
+    showCard.className = "card mb-3";
+    showCard.style = "max-width: 540px;";
+    const showCardRow = document.createElement("div");
+    showCardRow.className = "row no-gutters";
+
+    const showCardRowDiv1 = document.createElement('div');
+    showCardRowDiv1.className = 'col-md-4';
+    const showCardRowDiv1Img = document.createElement('img');
+    showCardRowDiv1Img.className = 'crd-img';
+    showCardRowDiv1Img.id = 'showCardImg';
+    showCardRowDiv1Img.src = show.image.original;
+    showCardRowDiv1Img.alt = 'This is the img of show';
+
+    const showCardRowDiv2 = document.createElement('div');
+    showCardRowDiv2.className = 'col-md-8';
+    const showCardRowDiv2Div1 = document.createElement('div');
+    showCardRowDiv2Div1.className = 'card-body';
+    const showCardTitle = document.createElement('h5');
+    showCardTitle.className = 'card-title';
+    showCardTitle.innerText = show.name;
+    const showCardSummary = document.createElement('p');
+    showCardSummary.className = 'card-text';
+    showCardSummary.innerText = show.summary;
+
+    const showCardBody = document.createElement('div');
+    showCardBody.className = 'card-body';
+    const showCardBodyUlElement = document.createElement('ul');
+    showCardBodyUlElement.className = 'list-group list-group-flush';
+
+    const showBodyLiElement1 = document.createElement('li');
+    showBodyLiElement1.className = 'list-group-item text-right';
+    showBodyLiElement1.innerHTML = `Genres: ${show.genres}`;
+
+    const showBodyLiElement2 = document.createElement('li');
+    showBodyLiElement2.className = 'list-group-item text-right';
+    showBodyLiElement2.innerHTML = `Status: ${show.status}`;
+
+    const showBodyLiElement3 = document.createElement('li');
+    showBodyLiElement3.className = 'list-group-item text-right';
+    showBodyLiElement3.innerHTML = `Rating: ${show.rating.average}`;
+
+    const showBodyLiElement4 = document.createElement('li');
+    showBodyLiElement4.className = 'list-group-item text-right';
+    showBodyLiElement4.innerHTML = `Runtime: ${show.runtime}`;
+
+
+    showContainer.appendChild(showCard);
+    showCard.appendChild(showCardRow);
+    showCardRow.appendChild(showCardRowDiv1);
+    showCardRowDiv1.appendChild(showCardRowDiv1Img);
+    showCardRow.appendChild(showCardRowDiv2);
+    showCardRowDiv2.appendChild(showCardRowDiv2Div1);
+    showCardRowDiv2Div1.appendChild(showCardTitle);
+    showCardRowDiv2Div1.appendChild(showCardSummary);
+    showCardRow.appendChild(showCardBody);
+    showCardBody.appendChild(showCardBodyUlElement);
+    showCardBodyUlElement.appendChild(showBodyLiElement1);
+    showCardBodyUlElement.appendChild(showBodyLiElement2);
+    showCardBodyUlElement.appendChild(showBodyLiElement3);
+    showCardBodyUlElement.appendChild(showBodyLiElement4);
+
+  });
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 // This function will populate the names of the shows by calling gelAllShows () function;
@@ -22,25 +98,26 @@ function createAndSelectMenuForShows() {
       return 1;
     }
     return 0;
-  })
+  });
 
-  const showDiv = document.querySelector("#firstSelectDiv");
-  //const selectShow = document.querySelector("#selectShows");
+  const selectShow = document.querySelector("#selectShows");
   sortedAllShows.forEach((show) => {
     const showOption = document.createElement("option");
     //showOption.setAttribute("value", show.id);
     showOption.value = show.id;
     showOption.innerText = show.name;
     selectShow.appendChild(showOption);
-
   });
 
-
-  selectShow.addEventListener("change", event => {
+  selectShow.addEventListener("change", (event) => {
     fetchEpisodesFromAPI(event.target.value);
   });
   fetchEpisodesFromAPI(sortedAllShows[0].id);
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// *
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -50,9 +127,8 @@ const fetchEpisodesFromAPI = (id) => {
   fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
     .then((response) => response.json())
     .then((data) => makePageForEpisodes(data))
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -157,15 +233,15 @@ function selectTheEpisodeFromList() {
     let selectedEpisodeString = selectedEpisode.join(" - ");
     const cardList = document.querySelectorAll(".card");
 
-    cardList.forEach(episode => {
+    cardList.forEach((episode) => {
       if (event.target.value === "All episodes") {
         episode.style.display = "block";
       } else {
-        episode.innerHTML.includes(selectedEpisodeString) ? episode.style.display = "block" : episode.style.display = "none";
+        episode.innerHTML.includes(selectedEpisodeString) ?
+          (episode.style.display = "block") :
+          (episode.style.display = "none");
       }
-
-    })
-
+    });
   }
 }
 
@@ -173,9 +249,9 @@ function selectTheEpisodeFromList() {
 
 // This function when called by the button clicked, the window will refresh with the episodes of the selected show.
 
-const clearWindowButton = document.querySelector('#clearWindow');
+const clearWindowButton = document.querySelector("#clearWindow");
 
-clearWindowButton.addEventListener('click', function () {
+clearWindowButton.addEventListener("click", function () {
   const divRow = document.querySelector("#cardContainer");
   divRow.innerHTML = "";
   const selectTheEpisodes = document.querySelector("#selectEpisodes");
@@ -185,8 +261,6 @@ clearWindowButton.addEventListener('click', function () {
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 window.onload = setup;
-
-
 
 /*
 get data from API;
